7dbd877aed8c0bde6795fe11ff2834e4727ef576
David Fisher <tibbon@gmail.com>
2014-02-10 16:02:21 -0500

    Ran rails new HackerNews --database=postgresl -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '740770f762271127b0933d2dd03312f358c0a2e3c89199d0e0c7a6f624f5db50484eba4e53b1457b196578db20d653003aef277113c8f44eccf3457ce11c6c4c'
