e45fcf702af01c861c669e65ee6a77d5c6dcc2ab
Prescott Murphy <prurph@gmail.com>
2014-02-13 09:10:22 -0500

    Remove shoulda matchers for built-in Rails validation

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe Story do
-  describe "validations" do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :link }
-    it { should validate_uniqueness_of :link }
-    it { should validate_presence_of :user_id }
-  end
-
   describe "associations" do
     it { should belong_to :user }
     it { should have_many :comments }
